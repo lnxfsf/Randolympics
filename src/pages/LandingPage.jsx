6e68337b5c42dacec46d6e2d55c6ad6bebf2387f
Linuxiness <igor.lerinc7@gmail.com>
2024-06-09 16:12:04 +0200

    tailwind installed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -20,6 +20,9 @@ const LandingPage = () => {
 
 
         <h1>hello</h1>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
 
 
     </>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+
+
+      pinky: "#EA1179",
+      blacky: "#111",
+
+      
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
