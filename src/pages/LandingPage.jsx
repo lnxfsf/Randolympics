d859337aa0d498f5ba0a6a5e37a388ce34c22ff8
Linuxiness <igor.lerinc7@gmail.com>
2024-06-10 13:47:51 +0200

    done most of landing page
    just to do footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -210,23 +210,13 @@ const LandingPage = () => {
       {/* start fourth part */}
 
       <div className="flex">
-
-
-
-
         {/* flex-wrap flex-col */}
         {/* text */}
         <div className="basis-1/2 flex justify-start items-center flex-wrap">
-
-
-        
-
-
-            {/* arrow */}
-            <div className="flex justify-start items-start mt-[450px] ml-16">
-                <img src="landing_page/arrow_down_2.png" />
-            </div>
-
+          {/* arrow */}
+          <div className="flex justify-start items-start mt-[450px] ml-16">
+            <img src="landing_page/arrow_down_2.png" />
+          </div>
 
           <div className="flex-wrap flex-col">
             <h2 className="text-2xl font-bold underline decoration-red_first pl-16 text-right">
@@ -234,38 +224,23 @@ const LandingPage = () => {
             </h2>
 
             <h1 className="text-[43px] pl-16 pt-8 text-right">
-              
-       
-       
-
-
-              Get elected and <br/> represent <span className="text-red_first  font-bold ">your</span> <br/>country
-
-
+              Get elected and <br /> represent{" "}
+              <span className="text-red_first  font-bold ">your</span> <br />
+              country
             </h1>
           </div>
-
-            
-
-
-
         </div>
 
-
-
-      {/* image */}
-      <div className="basis-1/2 flex justify-center items-center pr-32 pt-24 pb-16 ">
-        <img src="landing_page/other_images/2.png " className="image_landing" />
-      </div>
-
-
+        {/* image */}
+        <div className="basis-1/2 flex justify-center items-center pr-32 pt-24 pb-16 ">
+          <img
+            src="landing_page/other_images/2.png "
+            className="image_landing"
+          />
+        </div>
       </div>
 
       {/* end fourth part */}
-
-
-
-
 
       {/* start fifth part */}
       {/* image */}
@@ -295,6 +270,74 @@ const LandingPage = () => {
       </div>
 
       {/* end fifth part */}
+
+      {/* start about us part */}
+
+      <h1 className="flex text-[40px] mt-32 justify-center underline decoration-red_first">
+        About us
+      </h1>
+
+      <div className="flex ">
+        <div className="basis-1/2 pl-8 pt-16 text-justify">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+            <br /> <br />
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur. But I must explain to you how all this mistaken idea of
+            denouncing pleasure and praising pain was born and I will give you a
+            complete account of the system, and expound the actual teachings of
+            the great explorer of the truth, the master-builder of human
+            happiness. No one rejects, dislikes, or avoids pleasure itself,
+            because it is pleasure, but because those who do not know how to
+            pursue pleas. But I must explain to you how all this mistaken idea
+            of denouncing pleasure and praising pain was born and I will give{" "}
+            <br />
+          </p>
+        </div>
+
+        <div className="basis-1/2 flex justify-center items-center">
+          <img
+            src="landing_page/about_us.png"
+            className="image_landing_about_us"
+          />
+        </div>
+      </div>
+
+      <div className="p-8 text-justify">
+        But I must explain to you how all this mistaken idea of denouncing
+        pleasure and praising pain was born and I will give you a complete
+        account of the system, and expound the actual teachings of the great
+        explorer of the truth, the master-builder of human happiness. No one
+        rejects, dislikes, or avoids pleasure itself, because it is pleasure,
+        but because those who do not know how to pursue pleasure rationally
+        encounter consequences that are extremely painful. Nor again is there
+        anyone who loves or pursues or desires to obtain pain of itself, because
+        it is pain, but because occasionally circumstances occur in which toil
+        and pain can procure him some great pleasure. To take a trivial example,
+        which of us ever undertakes laborious physical exercise, except to
+        obtain some advantage from it? But who has any right to find fault with
+        a man who chooses to enjoy a pleasure that has no annoying consequences,
+        or one who avoids a pain that produces no resultant pleasure
+      </div>
+      {/* end about us part */}
 
       <h1>hello</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
