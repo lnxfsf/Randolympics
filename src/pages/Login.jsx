e218f9c9a699f89ee562211715544341816813a6
Linuxiness <igor.lerinc7@gmail.com>
2024-06-11 10:50:19 +0200

    captcha verify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,72 +3,76 @@ import "../styles/login.scoped.scss";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
-import ReCAPTCHA from 'react-google-recaptcha';
+import ReCAPTCHA from "react-google-recaptcha";
 
-import axios from 'axios';
+import axios from "axios";
 
-
-let BACKEND_SERVER_BASE_URL = import.meta.env.VITE_BACKEND_SERVER_BASE_URL || process.env.VITE_BACKEND_SERVER_BASE_URL;
-
-
+let BACKEND_SERVER_BASE_URL =
+  import.meta.env.VITE_BACKEND_SERVER_BASE_URL ||
+  process.env.VITE_BACKEND_SERVER_BASE_URL;
 
 const Login = () => {
-
-
-  const recaptcha = useRef()
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-
+  const recaptcha = useRef();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
 
   let loginUser = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    var email = e.target.email.value
-    var password = e.target.pass.value
-
-    
-
+    var email = e.target.email.value;
+    var password = e.target.pass.value;
 
     // check again, if email is correctly inserted
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
     if (!emailRegex.test(email)) {
-        alert("Please enter a valid email address");
+      alert("Please enter a valid email address");
     }
-    
 
     // check if captcha okay
-    const captchaValue = recaptcha.current.getValue()
+    const captchaValue = recaptcha.current.getValue();
+
     if (!captchaValue) {
-      alert('Please verify the reCAPTCHA!')
+      alert("Please verify the reCAPTCHA!");
     } else {
-      // make form submission
+      const res = await fetch("http://localhost:5000/captcha/verify", {
+        method: "POST",
+        body: JSON.stringify({ captchaValue }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
 
+      const data = await res.json();
+      if (data.success) {
+        // make form submission
 
-        let response = await axios.post(`${BACKEND_SERVER_BASE_URL}/login`, {email, password} );
-        
-        if(response){
-          alert("Login success ")
-        }else{
-          alert("Login failed")
+        let response = await axios.post(
+          `${BACKEND_SERVER_BASE_URL}/auth/login`,
+          { email, password }
+        );
+
+        if (response) {
+          alert("Login success ");
+        } else {
+          alert("Login failed");
         }
 
-        //  alert('Form submission successful!')
+       // alert("Form submission successful!");
+      } else {
+        alert("reCAPTCHA validation failed!");
+      }
+
+      // make form submission
     }
 
-
-
-
-
+    //  alert('Form submission successful!')
   };
 
-
-
-  let APP_SITE_KEY = import.meta.env.VITE_APP_SITE_KEY || process.env.VITE_APP_SITE_KEY;
-
-
+  let APP_SITE_KEY =
+    import.meta.env.VITE_APP_SITE_KEY || process.env.VITE_APP_SITE_KEY;
 
   return (
     <>
@@ -95,14 +99,10 @@ const Login = () => {
               <input type="password" id="pass" name="pass" required />
             </div>
 
-            
             <ReCAPTCHA ref={recaptcha} sitekey={APP_SITE_KEY} />
-
 
             {/*  this is for checkbox and forgot password*/}
             <div className="flex">
-
-
               <div className="basis-1/2">
                 <label htmlFor="remember">
                   <input
@@ -116,30 +116,27 @@ const Login = () => {
               </div>
 
               <div className="flex basis-1/2">
-                <Link to="/forgotpassword" className="bg-white text-red_first  ">
+                <Link
+                  to="/forgotpassword"
+                  className="bg-white text-red_first  "
+                >
                   Forgot Password?
                 </Link>
               </div>
-
-
-
-
             </div>
 
-
-
             <div className="flex">
-                <label htmlFor="tos">
-                  <input
-                    type="checkbox"
-                    id="tos"
-                    name="tos"
-                    className="mr-2"
-                  />
-                  I have read and understood the <Link to="/tos" className="text-red_first font-bold underline decoration-red_first">terms of service</Link>
-                </label>
-              </div>
-
+              <label htmlFor="tos">
+                <input type="checkbox" id="tos" name="tos" className="mr-2" />I
+                have read and understood the{" "}
+                <Link
+                  to="/tos"
+                  className="text-red_first font-bold underline decoration-red_first"
+                >
+                  terms of service
+                </Link>
+              </label>
+            </div>
 
             <div className="flex justify-center mt-8">
               <Button
