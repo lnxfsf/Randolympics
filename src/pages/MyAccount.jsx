8d0add651914d45a2cc82dd8b3b60c578528bc40
lnxfsf <igor.lerinc7@gmail.com>
2024-07-07 19:47:07 +0200

    error for unvalidated passport

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -14,10 +14,18 @@ import { PassportVrfy } from "../components/MyAccount/PassportVrfy";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+
 const MyAccount = () => {
   let { logoutUser } = useContext(AuthContext);
 
   const [user_type, setUserType] = useState("");
+  /*   const [passportStatus, setPassportStatus] = useState();
+  const [birthdate, setBirthdate] = useState(); */
+
+  const [warningMessage, setWarningMessage] = useState();
+  const [isRejected, setIsRejected] = useState(false);
+
 
   useEffect(() => {
     const storedData =
@@ -27,8 +35,21 @@ const MyAccount = () => {
       var userJson = JSON.parse(storedData);
 
       setUserType(userJson.data.user_type);
+
+     
+      /* setPassportStatus(userJson.data.setPassportStatus);
+      setBirthdate(userJson.data.birthdate); */
     }
-  }, []);
+
+    
+    warningBoxFunc(
+      userJson.data.passportStatus,
+      userJson.data.birthdate,
+      userJson.data.passport_photo
+    );
+
+   
+  }, [isRejected, warningMessage]);
 
   const [selectedItem, setSelectedItem] = useState("myAccount");
 
@@ -51,6 +72,38 @@ const MyAccount = () => {
     }
   };
 
+  const warningBoxFunc = (passportStatus, birthdate, passport_photo) => {
+    
+   
+    
+    if (passportStatus === "unvalidated") {
+      if (birthdate == null && passport_photo == null) {
+        setWarningMessage(
+          "Your passport is not verified. Upload passport photo and birthdate to get verified."
+        );
+      } else if (birthdate == null && passport_photo !== null) {
+        setWarningMessage(
+          "Your passport is not verified. Insert your birthdate to get verified."
+        );
+      } else if (birthdate !== null && passport_photo == null) {
+        setWarningMessage(
+          "Your passport is not verified. Upload passport photo to get verified."
+        );
+      } else {
+        setWarningMessage(
+          "Your passport is not yet verified. Waiting for approval.."
+        );
+      }
+    } else if (passportStatus === "rejected") {
+      setWarningMessage("Your passport is rejected. Reupload passport and birthdate.");
+      setIsRejected(true)
+
+      // TODO, red warning box, ! izgleda interesantnije ovaj scenario !
+    } else if (passportStatus === "validted"){
+      setWarningMessage("");
+      setIsRejected(false)
+    }
+  };
   return (
     <>
       <Navbar />
@@ -58,7 +111,7 @@ const MyAccount = () => {
       <div className="flex pl-8 pt-8 text-[35px] ">My Account</div>
 
       <div className="flex p-12 pl-2  pt-12 gap-4">
-        <div className="basis-1/3 side_nav p-4">
+        <div className="basis-1/3 side_nav p-4 ">
           <ul className="list flex flex-col">
             <li
               ref={myAccountRef}
@@ -152,6 +205,25 @@ const MyAccount = () => {
               Logout
             </li>
           </ul>
+
+          {warningMessage && (
+            <>
+             <div className={`flex flex-col p-2 mt-4 pl-2 ${isRejected===true ? 'error_box_rejected' : 'error_box'} `}  >
+             
+             
+             
+                <div className="flex">
+                  {/*  <img src="/myaccount/triangle-exclamation.svg" /> */}
+                  <WarningAmberIcon />
+                  <p className="pl-2">Warning !</p>
+                </div>
+
+                <div className="pl-8 pt-2">{warningMessage}</div>
+
+                <div className="pl-8 pt-4"> <p>You can't vote until your passport is verified.</p></div>
+              </div>
+            </>
+          )}
         </div>
 
         <div className="w-full">
