7647a213e0692a2777581219cd1b4803e45140e6
lnxfsf <igor.lerinc7@gmail.com>
2024-07-31 16:22:39 +0200

    news completed

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -7,6 +7,9 @@ const {
   blogs_upcominggames_picture_upload,
   revertBlogs_upcominggames_picture_upload,
 
+  blogs_news_picture_upload,
+  revertBlogs_news_picture_upload,
+
 
 } = require("../controllers/imageUploadControllers");
 const router = express.Router();
@@ -19,15 +22,21 @@ router.post("/passportPicture", passport_picture_upload);
 
 //  for upcoming games, blog
 router.post("/blogs_upcominggames_picture_upload", blogs_upcominggames_picture_upload)
+router.delete("/revertBlogs_upcominggames_picture_upload",revertBlogs_upcominggames_picture_upload)
+
 
 router.delete("/revertProfilePicture",revertProfilePicture)
 router.delete("/revertPassportPicture",revertPassportPicture)
-router.delete("/revertBlogs_upcominggames_picture_upload",revertBlogs_upcominggames_picture_upload)
 
 // for accessing routes to there.. (and later on, in frontend, you define more routes). if it's for profile, then add on this: "/uploads/profile_pictures"
 router.use("/profile_pics", express.static("uploads/profile_pictures"));
 router.use("/passport_pics", express.static("uploads/passport_pictures"));
 
+
+
+// for news blog
+router.post("/blogs_news_picture_upload", blogs_news_picture_upload)
+router.delete("/revertBlogs_news_picture_upload",revertBlogs_news_picture_upload)
 
 
 
