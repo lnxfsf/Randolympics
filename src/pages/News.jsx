e6769e80bc5fb3e1cb2c44d9642ada4c1c3e560c
lnxfsf <igor.lerinc7@gmail.com>
2024-08-01 13:42:51 +0200

    FAQ in it's component

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,19 @@
 
 
+import { FAQ } from "../components/Home/FAQ";
+import { NavbarHome } from "../components/NavbarHome";
+
+
+
 const News = () => {
 
     return (
         <>
+
+                <NavbarHome />
+
+                <FAQ />
+
 
 
         </>
