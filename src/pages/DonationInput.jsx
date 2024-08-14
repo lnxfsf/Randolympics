c816c5c520d8ef2f26029216456e91b1b936ea95
lnxfsf <igor.lerinc7@gmail.com>
2024-08-14 23:02:55 +0200

    button for payment, little wider

--- src/pages/DonationInput.jsx
+++ src/pages/DonationInput.jsx
@@ -2,20 +2,43 @@ import { CardContent, Grid, Typography, OutlinedInput, InputAdornment, Button, C
 
 
 const DonationInput = ({ amount, handleChange, handleSubmit, isLoading, error }) => (
+
     <CardContent>
         <Grid container spacing={2} justifyContent={"center"}>
             <Grid item xs={12} style={{ textAlign: "center" }}>
                 <Typography>Donate</Typography>
             </Grid>
-            <Grid item xs={6}>
+
+
+            {/* <Grid item xs={6}> */}
+            <Grid item xs={7}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <OutlinedInput
-                            type="text"
+                            type="number"
+
                             value={amount}
                             onChange={handleChange}
                             startAdornment={<InputAdornment position="start">$</InputAdornment>}
                             fullWidth
+
+                            inputProps={{ min: 1 }}
+
+                            sx={{
+                                // Hide arrows for WebKit browsers (Chrome, Safari, Edge, Opera)
+                                'input[type=number]::-webkit-inner-spin-button': {
+                                    WebkitAppearance: 'none',
+                                    margin: 0,
+                                },
+                                'input[type=number]::-webkit-outer-spin-button': {
+                                    WebkitAppearance: 'none',
+                                    margin: 0,
+                                },
+                                // Hide arrows for Firefox
+                                'input[type=number]': {
+                                    MozAppearance: 'textfield',
+                                },
+                            }}
                         />
                     </Grid>
                     <Grid item xs={12}>
