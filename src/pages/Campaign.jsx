341da74a62651a7c46c91e1a5017d3d38ddbae04
lnxfsf <igor.lerinc7@gmail.com>
2024-08-17 14:41:55 +0200

    DONE celebrity search by list all campaigns
    EVERYTHING WORKS UP TO HERE !
    
    and don't worry about, if there is no creator supporter, if he hasn't inserted email !
    that's because, only if he doesn't insert email initially, then we have no way to know who was initial creator supporter anyways.
    as anyone would be able to use same name, and we shouldn't flag it as creator..
    only based on his email, if he's left it. If not, then no way to have him there...

--- src/pages/Campaign.jsx
+++ src/pages/Campaign.jsx
@@ -22,8 +22,19 @@ import {
 
 import { NavbarHomeCollapsed } from "../components/NavbarHomeCollapsed";
 
+
+
+import Radio from "@mui/material/Radio";
+import FormLabel from "@mui/material/FormLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
+
+
+
 
 let FRONTEND_SERVER_BASE_URL =
   import.meta.env.VITE_FRONTEND_SERVER_BASE_URL ||
@@ -49,6 +60,12 @@ const Campaign = () => {
   const [searchPlaceholderFirstNameText, setSearchPlaceholderFirstNameText] =
     useState("first name");
 
+    // for celebrity
+  const [searchfb_link, setSearchfb_link] = useState(""); 
+  const [searchig_link, setSearchig_link] = useState(""); 
+  const [searchtw_link, setSearchtw_link] = useState(""); 
+  
+
   const [searchFamilyNameText, setSearchFamilyNameText] = useState(""); //search box
   const [searchPlaceholderFamilyNameText, setSearchPlaceholderFamilyNameText] =
     useState("family name");
@@ -62,6 +79,13 @@ const Campaign = () => {
   // popup
   const popupRef = useRef(null);
 
+
+  // do we filter by celebrity
+  const [filterIsCelebrity , setFilterIsCelebrity] = useState(0);
+
+
+
+
   useEffect(() => {
     updateLatestData();
   }, [
@@ -69,6 +93,10 @@ const Campaign = () => {
     filterNationality_selected,
     searchFirstNameText,
     searchFamilyNameText,
+    filterIsCelebrity,
+    searchfb_link,
+    searchig_link,
+    searchtw_link,
   ]);
 
   const updateLatestData = async () => {
@@ -84,6 +112,13 @@ const Campaign = () => {
             filterNationality_selected: filterNationality_selected,
             searchFirstNameText: searchFirstNameText,
             searchFamilyNameText: searchFamilyNameText,
+
+
+            isCelebrity: filterIsCelebrity,
+            fb_link: searchfb_link,
+            ig_link: searchig_link,
+            tw_link: searchtw_link,
+
           },
         }
       );
@@ -101,6 +136,8 @@ const Campaign = () => {
     setSearchFirstNameText("");
 
     setSearchFamilyNameText("");
+    setFilterIsCelebrity(0);
+
   };
 
   return (
@@ -169,6 +206,40 @@ const Campaign = () => {
               <span className="popins-font">Reset fields</span>
             </Button>
 
+
+            <FormControl >
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue={()=> {if(filterIsCelebrity===0){
+                  return "no"
+                }else {
+                  return "yes"
+                }}}
+                name="radio-buttons-group"
+                onChange={(event) => {
+                  const value = event.target.value;
+
+                  if (value === "yes") {
+                    setFilterIsCelebrity(1);
+                    
+                  } else if (value === "no") {
+                    setFilterIsCelebrity(0);
+                   
+                  }
+                }}
+              >
+                <FormGroup row>
+                  <FormControlLabel
+                    value="yes"
+                    control={<Radio />}
+                    label={`Yes`}
+                  />
+                  <FormControlLabel value="no" control={<Radio />} label={`No`} />
+                </FormGroup>
+              </RadioGroup>
+            </FormControl>
+           
+
             <FormControl
               variant="standard"
               sx={{ m: 1, minWidth: 120 }}
@@ -205,6 +276,10 @@ const Campaign = () => {
               placeholder="Nationality"
             />
 
+
+      {/* this is for athlete NON celebrity */}
+      {filterIsCelebrity === 0 && (
+      <div>
             <div className="flex items-start flex-col">
               <p>First name</p>
               <SearchBar
@@ -234,6 +309,80 @@ const Campaign = () => {
                 }}
               />
             </div>
+      </div>
+)}
+
+
+  {/* this is for athlete CELEBRITY */}
+  {filterIsCelebrity === 1 && (
+      <div>
+
+<div className="flex items-start flex-col">
+              <p>First name</p>
+              <SearchBar
+                value={searchFirstNameText}
+                onChange={(newValue) => setSearchFirstNameText(newValue)}
+                onCancelResearch={(newValue) => setSearchFirstNameText("")}
+                placeholder={"Search " + searchPlaceholderFirstNameText}
+                onSearch={handleSearch}
+                style={{
+                  border: "1px solid #C6C6C6", // Border color and thickness
+                  borderRadius: "20px", // Border radius
+                }}
+              />
+            </div>
+
+
+            <div className="flex items-start flex-col">
+              <p>Facebook link</p>
+              <SearchBar
+                value={searchfb_link}
+                onChange={(newValue) => setSearchfb_link(newValue)}
+                onCancelResearch={(newValue) => setSearchfb_link("")}
+             /*  placeholder={""}  */
+                onSearch={handleSearch}
+                style={{
+                  border: "1px solid #C6C6C6", // Border color and thickness
+                  borderRadius: "20px", // Border radius
+                }}
+              />
+            </div>
+
+
+            <div className="flex items-start flex-col">
+              <p>Instagram link</p>
+              <SearchBar
+                value={searchig_link}
+                onChange={(newValue) => setSearchig_link(newValue)}
+                onCancelResearch={(newValue) => setSearchig_link("")}
+                /* placeholder={"Search " + searchPlaceholderFirstNameText} */
+                onSearch={handleSearch}
+                style={{
+                  border: "1px solid #C6C6C6", // Border color and thickness
+                  borderRadius: "20px", // Border radius
+                }}
+              />
+            </div>
+
+
+            <div className="flex items-start flex-col">
+              <p>Twitter (X) link</p>
+              <SearchBar
+                value={searchtw_link}
+                onChange={(newValue) => setSearchtw_link(newValue)}
+                onCancelResearch={(newValue) => setSearchtw_link("")}
+               /*  placeholder={"Search " + searchPlaceholderFamilyNameText} */
+                onSearch={handleSearch}
+                style={{
+                  border: "1px solid #C6C6C6", // Border color and thickness
+                  borderRadius: "20px", // Border radius
+                }}
+              />
+            </div>
+      </div>
+)}
+
+
           </div>
         </Popup>
 
@@ -256,8 +405,6 @@ const Campaign = () => {
           {campaigns.map((item, index) => (
             <>
               <div className="flex justify-center items-center ">
-             
-             
                 <div
                   key={index}
                   /*   className="flex justify-between border-2 m-4 p-2 select-none cursor-pointer" */
@@ -275,17 +422,19 @@ const Campaign = () => {
                     </p>
                   </div>
 
-                {item.isCelebrity && (
-                  <img className=" ml-auto w-6 m-4" src="/supporters/celebrity_icon.svg"  />
-                )}
+                  {item.isCelebrity ? (
+                    <img
+                      className=" ml-auto w-6 m-4"
+                      src="/supporters/celebrity_icon.svg"
+                    />
+                  ) : (
+                    <></>
+                  )}
 
                   <div>
                     <Flag className="w-12 " code={item.friendNationality} />
                   </div>
                 </div>
-
-
-
               </div>
             </>
           ))}
