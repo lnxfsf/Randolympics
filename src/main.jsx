41b893750cb97944cf3ff331dd4ee67a2d01d1f9
lnxfsf <igor.lerinc7@gmail.com>
2024-09-10 09:21:44 +0200

    react-snap trying to get SEO work

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { hydrate, render } from "react-dom";
+
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
@@ -10,10 +12,38 @@ import "./index.css";
 import '@mui/material/styles/styled';
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+const app = (
   <Router>
     <AuthProvider>
       <App />
     </AuthProvider>
   </Router>
 );
+
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(app, rootElement);
+} else {
+  render(app, rootElement);
+}
+
+
+/* before  installing  react-snap (for SEO)
+
+this is in: package.json:
+"postbuild": "react-snap",
+
+/ ----- 
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>
+); 
+ */
