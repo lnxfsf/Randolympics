16f0c5c97fa775d54e3c239f452057182f65d6d9
lnxfsf <igor.lerinc7@gmail.com>
2024-09-17 19:59:11 +0200

    settings part

--- src/components/MyAccount/Settings.jsx
+++ src/components/MyAccount/Settings.jsx
@@ -164,122 +164,58 @@ const Settings = () => {
 
 
 
-
-  const handleBioChange = (event) => {
-    setBio(event.target.value);
-
-    setUserData((prevUserData) => ({
-      ...prevUserData,
-      data: {
-        ...prevUserData.data,
-        bio: event.target.value,
-      },
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      var response = await axios.post(
-        `${BACKEND_SERVER_BASE_URL}/user/update_user_data`,
-        {
-          original_email,
-          bio: bio,
-        }
-      );
-
-      if (response.status === 200) {
-        if (localStorage.getItem("authTokens")) {
-          localStorage.setItem("authTokens", JSON.stringify(userData));
-        } else if (sessionStorage.getItem("authTokens")) {
-          sessionStorage.setItem("authTokens", JSON.stringify(userData));
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
-      <HeaderMyProfile  />
-      <div className="flex flex-col">
-        <form action="#" onSubmit={handleSubmit}>
-         
-         
-          <div className="flex flex-col w-full h-32 mr-4 mt-2">
-            <TextField
-              label="About me"
-              value={userData && userData.data.bio}
-              onChange={handleBioChange}
-              placeholder="Bio"
-              id="bio"
-              name="bio"
-              multiline
-              rows={4}
-              className="w-full h-full rounded-md border border-gray-900"
-              type="text"
-              sx={{
-                m: 1,
-                /* width: "2px",  */
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: 5, // Rounded corners
-                },
 
-                "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
-                  {
-                    borderColor: "red", // Red border on focus
-                  },
-
-                "& .MuiInputLabel-root": {
-                  "&.Mui-focused": {
-                    color: "black", // Set label color to black when focused
-                  },
-                },
-              }}
-              inputProps={{
-                maxLength: 255,
-              }}
-            />
-          </div>
+      <p className="lexend-font text-black_second font-bold text-xl md:text-2xl p-2">Settings</p>
+      <div className="flex flex-col p-2">
 
 
 
+      <div className="rounded-md lexend-font text-black_second select-none font-medium"
+      style={{border: "1.5px solid #C6C6C6"}}
+      >
+
+        <div className="h-8 flex justify-start items-center gap-2 cursor-pointer">
+
+          <img src="/editprofile/withdraw.svg"  className="w-5 ml-4" />
+          <p>Withdraw Candidacy</p>
 
 
 
+        </div>
 
-{/* don't show this for other users.. no point, for now.. JUST FOR   NP's and GP's  */}
+        <hr style={{border: "1px solid #C6C6C6"}}/>
+
+        <div className="h-8 flex justify-start items-center gap-2 cursor-pointer">
+
+          <img src="/editprofile/resign.svg"  className="w-4 ml-4" />
+          <p>Resign from this position</p>
+
+        </div>
+
+
+
+        <hr style={{border: "1px solid #C6C6C6"}}/>
+
+<div className="h-8 flex justify-start items-center gap-2 cursor-pointer">
+
+  <img src="/editprofile/delete.svg"  className="w-4 ml-4" />
+  <p className="text-[#FD5757]">Delete Account</p>
+
+
+</div>
+
+
+      </div>
+
+
+      
+
+
           <p className="m-4 mt-6">
             Would you like to resign from current position ?{" "}
           </p>
-
-
-
-         {/*   <Button
-           onClick={resignFromPosition}
-            className="w-[100px] "
-            style={{ marginTop: "5px" }}
-            sx={{
-              marginLeft: "10px",
-              fontSize: "8pt",
-              height: "40px",
-              bgcolor: "#AF2626",
-              color: "#fff",
-              borderRadius: 15,
-              border: `1px solid #AF2626`,
-              "&:hover": {
-                background: "rgb(196, 43, 43)",
-                color: "white",
-                border: `1px solid rgb(196, 43, 43)`,
-              },
-            }}
-            variant="text"
-            value="resign"
-          >
-            <span className="popins-font">Resign</span>
-          </Button> */}
 
           <Popup
                   ref={popupRefResign}
@@ -461,53 +397,8 @@ const Settings = () => {
 
 
 
-            <div className="flex gap-2 items-center self-center">
-              <Button
-                onClick={handleCancel}
-                className="w-[200px]"
-                style={{ marginTop: "0px" }}
-                sx={{
-                  height: "50px",
-                  bgcolor: "#fff",
-                  color: "#000",
-                  borderRadius: 15,
-                  border: `1px solid #AF2626`,
-                  "&:hover": {
-                    background: "rgb(196, 43, 43)",
-                    color: "white",
-                    border: `1px solid rgb(196, 43, 43)`,
-                  },
-                }}
-                variant="text"
-                value="Login"
-                id="login-btn"
-              >
-                <span className="popins-font">Cancel</span>
-              </Button>
-
-              <Button
-                className="w-[200px]"
-                style={{ marginTop: "0px" }}
-                sx={{
-                  height: "50px",
-                  bgcolor: "#AF2626",
-                  color: "#fff",
-                  borderRadius: 15,
-                  border: `1px solid #AF2626`,
-                  "&:hover": {
-                    background: "rgb(196, 43, 43)",
-                    color: "white",
-                    border: `1px solid rgb(196, 43, 43)`,
-                  },
-                }}
-                type="submit"
-                variant="text"
-              >
-                <span className="popins-font">Save</span>
-              </Button>
-            </div>
+        
           </div>
-        </form>
       </div>
     </>
   );
