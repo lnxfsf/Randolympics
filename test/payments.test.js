e6ee537f40cc1f5320d0cefd8d8005e09d2f3887
lnxfsf <igor.lerinc7@gmail.com>
2024-10-13 12:27:37 +0200

    normal payment test works!

--- test/payments.test.js
+++ test/payments.test.js
@@ -7,6 +7,8 @@ const dbConfig = require("../data/config"); */
 const db = require("../models/database");
 
 const Statscampaign = db.statscampaign;
+const Campaign = db.campaign;
+const Users = db.users;
 
 const generateRandomEmail = () => {
   const timestamp = Date.now();
@@ -18,6 +20,128 @@ const generateRandomEmail = () => {
 // Function to create a delay
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+
+const campaignId = uuidv4();
+
+let friendEmail;
+let supporterEmail;
+
+
+describe("creating campaign for friend, that will be used for all payments tests", () => {
+
+  before(() => {
+    friendEmail = generateRandomEmail();
+    supporterEmail = generateRandomEmail();
+  });
+
+  it("should make campaign for friend (saved in 'Campaigns' table)", function (done) {
+    superagent
+      .post("http://localhost:5000/listsData/createCampaign")
+      .send({
+        campaignId,
+        friendName: "test_name_friend",
+        friendMiddleName: "test_middleName_friend",
+        friendLastName: "test_friendLastName_friend",
+        friendFamilyName: "test_friendFamilyName_friend",
+        friendEmail,
+        friendPhone: "+36285921926",
+        friendBirthdate: "1995-09-27",
+        friendNationality: "US",
+        friendImage: "ariana_profile.jpg",
+        friendGender: "F",
+
+        supporterName: "test_supporterName_friend",
+        supporterPhone: "+36285921926",
+        supporterEmail,
+        supporterComment: "Go go go (supporter comment test)",
+
+        isCelebrity: false,
+        fb_link: "",
+        ig_link: "",
+        tw_link: "",
+      })
+      .end(function (err, res) {
+        if (res.status === 201) {
+          console.log(`Campaign id is: ${campaignId}`);
+          done();
+        } else {
+          done(err);
+        }
+      });
+  });
+
+  it("after creating campaign (for friend), it should create (register) account as well (as there's none, and campaign can't be created if (athlete) account exists)", function (done) {
+    superagent
+      .post("http://localhost:5000/auth/register")
+      .send({
+        email: friendEmail,
+        password: "12345678",
+        user_type: "AH",
+        name: "test_name_friend",
+        middleName: "test_middleName_friend",
+        lastName: "test_friendLastName_friend",
+        phone: "+36285921926",
+        nationality: "US",
+        weight: "53",
+        cryptoaddress: "fsfdsklčcx43242sfsad",
+        picture: "ariana_profile.jpg",
+        cryptoaddress_type: "BTC",
+        bio: "Test bio athlete campaign",
+        gender: "F",
+
+        signedByFriend: true,
+
+        supporterName: "test_name_supporter",
+        campaignURL: `http://localhost:5000/campaign/${campaignId}`,
+
+        sendEmailToFriend: true,
+
+        isCelebrity: false,
+        fb_link: "",
+        ig_link: "",
+        tw_link: "",
+      })
+      .end(function (err, res) {
+        if (res.status === 201) {
+          done();
+        } else {
+          done(err);
+        }
+      });
+  });
+
+  it("also, when supporter inserts his info in campaign, that gets saved. And we create supporter account, with user_type='SPT'", function (done) {
+    superagent
+      .post("http://localhost:5000/auth/register")
+      .send({
+        email: supporterEmail,
+        password: "12345678",
+        user_type: "SPT",
+        name: "test_name_supporter",
+        phone: "+36285921926",
+        weight: "55",
+        cryptoaddress: "",
+        picture: "ariana_profile.jpg",
+        cryptoaddress_type: "BTC",
+        bio: "",
+        gender: "F",
+        supporterComment: "Go go go (supporter comment test)",
+        campaignURL: `http://localhost:5000/campaign/${campaignId}`,
+        signingAsSupporter: true,
+      })
+      .end(function (err, res) {
+        if (res.status === 201) {
+          done();
+        } else {
+          done(err);
+          
+        }
+      });
+  });
+
+});
+
+
 describe("payments", () => {
   let randomEmail;
 
@@ -26,8 +150,21 @@ describe("payments", () => {
   before(async () => {
     randomEmail = generateRandomEmail();
   });
+
+
+
+
+
+
+
   it("Anonymous 10$ payment without any donations (regular pay with credit card) (campaignId is not of real campaign)", async function () {
     this.timeout(15000);
+
+       // and last, check if "how many supporter" works, by just fetching from "Statscampaigns" (transactions), for my campaignId, and see how much you got. compared to previous. (this will get added more and more, throughout test, for other payments.. so you check this as well now)
+       const prevHowManySupporters = await Statscampaign.count({
+        where: { campaignId: campaignId },
+      });
+
 
     const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -37,7 +174,7 @@ describe("payments", () => {
       .set("Content-Type", "application/json")
       .send({
         amount: "10",
-        campaignId: "eecad191-2a6e-4dd7-9280-d85aec85f288",
+        campaignId,
         supporterName: "",
         supporterEmail: "",
         supporterComment: "",
@@ -47,7 +184,7 @@ describe("payments", () => {
 
     const { paymentIntent } = res.body;
 
-    if (!paymentIntent) {
+    if (!paymentIntent.id) {
       throw new Error(
         "paymentIntent not received. Have you run Stripe CLI in background? `stripe listen --forward-to localhost:5000/webhook` "
       );
@@ -62,17 +199,36 @@ describe("payments", () => {
       }
     );
 
+
+     // also check in athlete itself, if amount calculated is right one?
+    // first find the campaign
+    const campaignCheck = await Campaign.findOne({
+      where: { campaignId: campaignId },
+    });
+  
+
+    // and then extract friendEmail, that's that athlete email used
+    const athleteCheck = await Users.findOne({
+      where: { email: campaignCheck.friendEmail },
+    });
+
+    let prevDonatedAmountAH = athleteCheck.donatedAmount;
+
     // Query the database to check if paymentIntent.id is saved
     // first, check if payment_id is written (so immediatelly)
     const paymentIdCheck = await Statscampaign.findOne({
       where: { payment_id: paymentIntent.id },
     });
 
+
+ 
+
     if (!paymentIdCheck) {
         throw new Error(
           "payment_id not found in database. So payment will fail further."
         );
     }
+
 
     // and then wait for some time, and check database again, 
     await delay(5000);
@@ -81,6 +237,41 @@ describe("payments", () => {
     const paymentCheckProcessed = await Statscampaign.findOne({
       where: { payment_id: paymentIntent.id },
     });
+
+     // it needs to check database again, for newest data
+     const athleteCheckAgain = await Users.findOne({
+      where: { email: campaignCheck.friendEmail },
+    });
+   
+
+   // console.log("ss"+athleteCheck.donatedAmount)
+
+   // now, confirm, that donatedAmount (current previous), is same as one you get after 
+   // only if this is NOT the case, it throws error, and fails test
+     if(!(prevDonatedAmountAH + 1000 === athleteCheckAgain.donatedAmount)){
+      // 10$, you see it as 1000 (last 2 digits are cents)
+      throw new Error(
+        `Previous amount '${prevDonatedAmountAH}' in athlete (table Users) is not same calculation what it should be. It's ${athleteCheck.donatedAmount}`
+      );
+
+    } 
+
+
+    
+    // and last, check if "how many supporter" works, by just fetching from "Statscampaigns" (transactions), for my campaignId, and see how much you got. compared to previous. (this will get added more and more, throughout test, for other payments.. so you check this as well now)
+    const howManySupporters = await Statscampaign.count({
+      where: { campaignId: campaignId },
+    });
+
+
+    if (prevHowManySupporters === howManySupporters){
+      throw new Error(
+        "'how many supporters', doesn't work. transaction is not written in 'Statcampaign' table, and/or campaignId is not used to indentify for which campaign transaction is connected to. "
+      );
+    }
+
+
+
 
 
     // so payment_status === "payment_status" && amount > 0, to know payment really worked fine
@@ -92,6 +283,8 @@ describe("payments", () => {
             `payment didn't proceed through (not written in database). payment_id is '${paymentIdCheck.payment_id}' , so check it directly in MySQL database once again 'select * from statscampaigns where payment_id="<payment_id>";'. Delay in database is 5 seconds, before checking database once again, if payment went through. `
           );
     }
+
+
 
 
   });
