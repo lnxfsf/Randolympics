0b11ead5e0436bb4794ce06d4f101c58e7106c86
lnxfsf <igor.lerinc7@gmail.com>
2024-11-02 13:54:06 +0100

    gets list of campaigns by this user

--- src/components/MyAccount/CreatedCampaigns.jsx
+++ src/components/MyAccount/CreatedCampaigns.jsx
@@ -1,10 +1,84 @@
+import axios from "axios";
+import { useEffect, useState, useRef } from "react";
+import SearchBar from "@mkyy/mui-search-bar";
+
+import { Button, Avatar } from "@mui/material";
+
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+import { useTranslation } from "react-i18next";
+
+let BACKEND_SERVER_BASE_URL =
+  import.meta.env.VITE_BACKEND_SERVER_BASE_URL ||
+  process.env.VITE_BACKEND_SERVER_BASE_URL;
+
 const CreatedCampaigns = () => {
+  const { t } = useTranslation();
 
-    return (<>
+  const [maxPages, setMaxPages] = useState(0);
+  const [resultsAmount, setResultsAmount] = useState();
 
-    ddd
-    </>)
+  const [campaigns, setCampaigns] = useState();
+  const [campaignsPage, setCampaignsPage] = useState(1);
 
-}
+  const [limit, setLimit] = useState(10);
 
-export {CreatedCampaigns}
+  const [searchFirstNameText, setSearchFirstNameText] = useState(""); //search box
+
+  const [currentUserId, setCurrentUserId] = useState(() => {
+    const storedData =
+      localStorage.getItem("authTokens") ||
+      sessionStorage.getItem("authTokens");
+
+    if (storedData) {
+      const userJson = JSON.parse(storedData);
+
+      return userJson.data.userId;
+    }
+  });
+
+  const handleSearch = (he) => {
+    // Fired when enter button is pressed.
+  };
+
+  useEffect(() => {
+    updateLatestData();
+  }, [searchFirstNameText]);
+
+  const handlePaginationChange = (event, value) => {
+    setCampaignsPage(value);
+  };
+
+  const updateLatestData = async () => {
+    try {
+      const response = await axios.get(
+        `${BACKEND_SERVER_BASE_URL}/listsData/listCreatedCampaignsByUser`,
+        {
+          params: {
+            limit: limit,
+            offset: (campaignsPage - 1) * 10,
+
+            currentUserId,
+
+            searchFirstNameText: searchFirstNameText,
+          },
+        }
+      );
+
+      setMaxPages(Math.ceil(response.data.count / 10));
+      setResultsAmount(response.data.count);
+
+      setCampaigns(response.data.rows);
+
+      console.log("campaigns for this user");
+      console.log(response.data.rows);
+    } catch (e) {
+      console.log(e.stack);
+    }
+  };
+
+  return <></>;
+};
+
+export { CreatedCampaigns };
