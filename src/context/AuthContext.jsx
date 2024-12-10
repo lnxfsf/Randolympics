6bbdbff81004ff0764eefbca8efcc8be98bc5f7d
lnxfsf <igor.lerinc7@gmail.com>
2024-12-10 15:24:01 +0100

    jwt_auth_better

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 const AuthContext = createContext();
-import { useCallback } from 'react';
+import { useCallback } from "react";
 
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
@@ -13,14 +13,12 @@ let BACKEND_SERVER_BASE_URL =
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
-
-  
-  
   let [campaignId, setCampaignId] = useState("");
-  
 
+
+  
   const [user, setUser] = useState(() => {
-    const tokenString =
+    /* const tokenString =
       localStorage.getItem("authTokens") ||
       sessionStorage.getItem("authTokens");
 
@@ -33,122 +31,176 @@ export const AuthProvider = ({ children }) => {
         // localStorage.removeItem("authTokens");
         // return null;
       }
-    }
+    } */
     // return null;
   });
-
+/* 
   let [authTokens, setAuthTokens] = useState(() => {
     let authTokensString =
       (typeof localStorage !== "undefined" &&
         localStorage.getItem("authTokens")) ||
       (typeof sessionStorage !== "undefined" &&
         sessionStorage.getItem("authTokens"));
-
+// if authTokens is not empty in local/session storage, then save their values in this 
     return authTokensString ? JSON.parse(authTokensString) : null;
-  });
-
-  let [loading, setLoading] = useState(true);
+  }); */
 
   const navigate = useNavigate();
-
-
-
 
   let loginUser = async (email, password, remember_me) => {
     try {
       var response = await axios.post(`${BACKEND_SERVER_BASE_URL}/auth/login`, {
         email,
         password,
-      });
-      
+      }, {withCredentials: true }); // it needs withCredentials: true, so it can receive and setCookie refreshToken
     } catch (error) {
       if (error.response.status === 401) {
-       // alert(error.response.data.message);
-       return 0;
+        // alert(error.response.data.message);
+        return 0;
       } else {
         console.log(error);
         return 0;
       }
     }
 
-   
-
     // AND HERE, YOU CAN DECIDE WHETHER IT IS local or session storage, depending on whether "remember me" is selected or not..
     if (response) {
+
+
+      // TODO this is for now, but we shouldn't even return all these stuff, but just jwt token directly.. so wait until i refactor everything else, to comply with better jwt security practices
+      const withoutAccessToken = { ...response }; // Shallow copy of response
+
+       withoutAccessToken.data = Object.fromEntries(
+        Object.entries(response.data).filter(([key]) => key !== 'access_token')
+      );
+
+
+
       if (remember_me) {
-        localStorage.setItem("authTokens", JSON.stringify(response));
+
+        // set accessToken
+        localStorage.setItem("accessToken", response.data.access_token);
+
+        // this is where we actually save in localstorage (we actually directly access local/session storage in app whenever we need it)
+        localStorage.setItem("authTokens", JSON.stringify(withoutAccessToken));
+
+
+
       } else {
-        sessionStorage.setItem("authTokens", JSON.stringify(response));
+         // set accessToken
+         sessionStorage.setItem("accessToken", response.data.access_token);
+
+        // this is where we actually save in sessionstorage 
+        sessionStorage.setItem("authTokens", JSON.stringify(withoutAccessToken));
       }
 
+
+/* 
       setAuthTokens(response);
+ */
 
-
+      // it gets userId, directly from JWT token payload
       setUser(jwtDecode(response.data.access_token));
 
 
-      // here check if athleteStatus is "s1" or "s2", then it redirects to that screen where user inserts status (on that, once it's filled, then, we redirect to home, or myprofile). and this screen is shown only for athlete user type ! 
-      if((response.data.athleteStatus === "s1" || response.data.athleteStatus === "s2") && response.data.user_type === "AH" ){
-        
+      // here check if athleteStatus is "s1" or "s2", then it redirects to that screen where user inserts status (on that, once it's filled, then, we redirect to home, or myprofile). and this screen is shown only for athlete user type !
+      if (
+        (response.data.athleteStatus === "s1" ||
+          response.data.athleteStatus === "s2") &&
+        response.data.user_type === "AH"
+      ) {
         navigate("/updateAthleteStatus");
-        
       } else {
-        
-          navigate("/");
-          
-        }
-
+        navigate("/");
+      }
     } else {
-      
       return 0;
     }
-
-   
-
-    
   };
+
+
+
+  const refreshAccessToken = async () => {
+    try {
+      const response = await axios.post(
+        `${BACKEND_SERVER_BASE_URL}/auth/refresh`,
+        {},
+        { withCredentials: true } //  cookies are sent
+      );
+
+      const { access_token } = response.data;
+
+      // Update accessToken in storage
+      if (localStorage.getItem("accessToken")) {
+        localStorage.setItem("accessToken", access_token);
+      } else if (sessionStorage.getItem("accessToken")) {
+        sessionStorage.setItem("accessToken", access_token);
+      }
+
+
+      setUser(jwtDecode(access_token)); // set userId again, but we don't need really this, just ...
+
+      return access_token;
+
+    } catch (error) {
+      console.error("Failed to refresh access token:", error);
+      logoutUser(); // Log out if refresh fails, then we don't have latest credentials
+    }
+  };
+
+
+  useEffect(() => {
+    const REFRESH_INTERVAL = 4 * 60 * 1000; // 4 mins
+
+    const interval = setInterval(() => {
+      refreshAccessToken();
+    }, REFRESH_INTERVAL);
+
+    return () => clearInterval(interval); 
+  }, []);
+
+
+
 
   // just call this function, for logout, and you're done
   let logoutUser = (e) => {
     //e.preventDefault();
     localStorage.removeItem("authTokens");
     sessionStorage.removeItem("authTokens");
-    setAuthTokens(null);
+
+    localStorage.removeItem("accessToken");
+    sessionStorage.removeItem("accessToken");
+    
+   // setAuthTokens(null);
     setUser(null);
     navigate("/");
   };
 
-
-
   // TODO for campaignID ! I should've moved in separate context
 
-  let settingCampaignId = useCallback((id) => {
-    setCampaignId(id);
-  }, [campaignId]);
+  let settingCampaignId = useCallback(
+    (id) => {
+      setCampaignId(id);
+    },
+    [campaignId]
+  );
 
   let thisNow = (id) => {
     settingCampaignId(id);
-  }
-
+  };
 
   let contextData = {
     user: user,
-    authTokens: authTokens,
+   // authTokens: authTokens,
     loginUser: loginUser,
     logoutUser: logoutUser,
 
-   // campaignId: campaignId,
+    // campaignId: campaignId,
     //setCampaignId: setCampaignId,
-  //  settingCampaignId: thisNow,
-
-    
+    //  settingCampaignId: thisNow,
   };
-
-
 
   return (
     <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
-
-
   );
 };
