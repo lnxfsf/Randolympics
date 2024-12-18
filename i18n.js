a31d718e53e5f34456eed00f8a19746ff251bfa1
lnxfsf <igor.lerinc7@gmail.com>
2024-12-18 13:09:07 +0100

    origin

--- i18n.js
+++ i18n.js
@@ -25,10 +25,61 @@ i18n
     escapeValue: false,
   },
   backend: {
-    loadPath: 'https://randolympics.fra1.cdn.digitaloceanspaces.com/locales/{{lng}}/{{ns}}.json', // Path to the translation files
+// bilo je samo //  loadPath: '/locales/{{lng}}/{{ns}}.json', // Path to the translation files
+   
+    loadPath: 'https://randolympics.fra1.digitaloceanspaces.com/locales/{{lng}}/{{ns}}.json', // Path to the translation files
+   
     crossDomain: true
   },
   
 });
 
 export default i18n;
+
+
+
+
+// old file
+/* 
+
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+
+let S3_BUCKET_CDN_BASE_URL =
+import.meta.env.VITE_S3_BUCKET_CDN_BASE_URL ||
+process.env.VITE_S3_BUCKET_CDN_BASE_URL;
+
+
+
+
+
+
+
+i18n
+.use(initReactI18next)
+.use(LanguageDetector)
+.use(Backend)
+.init({
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+  backend: {
+     loadPath: '/locales/{{lng}}/{{ns}}.json', // Path to the translation files
+   
+   
+    crossDomain: true
+  },
+  
+});
+
+export default i18n;
+
+
+
+*/
