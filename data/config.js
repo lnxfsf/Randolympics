8cb7c24991321c8c2afceeabe2139518f1b4aefd
ragacijane <lukajanke23@gmail.com>
2024-12-22 13:44:25 +0100

    lets role

--- data/config.js
+++ data/config.js
@@ -1,30 +1,24 @@
-
-
 const config = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    dialect: 'mysql',
-    port: process.env.PORT_MYSQL,
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  dialect: "mysql",
+  port: process.env.PORT_MYSQL,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-   
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-  
-  module.exports = config;
+  },
 
-  
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
 
-  
+module.exports = config;
