72cc94bd839412fbe2caeb5bff9e9d9e78a205a1
lnxfsf <igor.lerinc7@gmail.com>
2024-12-23 02:36:07 +0100

    email service done

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,30 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 module.exports = async (email, subject, text) => {
-    try {
-        const transporter = nodemailer.createTransport({
-             
-            service: process.env.SERVICE,
-            
-            auth: {
-                user: process.env.USER_email,
-                pass: process.env.PASS_email
-            }
-        });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
 
-        // here he sends mail
-        await transporter.sendMail({
-            from: process.env.USER_email,
-            to: email,
-            subject: subject,
-            html: text
-        });
+      secure: true,
 
-        console.log("Email sent successfully");
-    } catch (err) {
-        console.error("Error sending email:");
-        console.error(err);
-    }
+      auth: {
+        user: process.env.USER_email,
+        pass: process.env.PASS_email,
+      },
+    });
+
+    // here he sends mail
+    await transporter.sendMail({
+      from: process.env.USER_email,
+      to: email,
+      subject: subject,
+      html: text,
+    });
+
+    console.log("Email sent successfully");
+  } catch (err) {
+    console.error("Error sending email:");
+    console.error(err);
+  }
 };
