d73a22ed27fda4f886fbb31982e526dc44407d3f
lnxfsf <igor.lerinc7@gmail.com>
2024-12-23 15:25:08 +0100

    added ToS

--- src/pages/ToS.jsx
+++ src/pages/ToS.jsx
@@ -2,34 +2,54 @@ import { useEffect } from "react";
 import { FooterClean } from "../components/FooterClean";
 import { Navbar } from "../components/Navbar";
 import { NavbarClean } from "../components/NavbarClean";
-
+import { useTranslation } from "react-i18next";
 const ToS = () => {
+  const { t } = useTranslation();
 
-
-  useEffect(()=>{
-
+  useEffect(() => {
     const hash = window.location.hash;
-    if(hash === "#termsof"){
+    if (hash === "#termsof") {
       const element = document.getElementById("termsof");
       if (element) {
         element.scrollIntoView({ behavior: "smooth" });
       }
-      
     }
-  },[])
-
-
+  }, []);
 
   return (
     <>
-     
       <Navbar />
 
       <div className="m-8">
-       
+        <p className="text-4xl">{t("privacyPolicy.ToSTitle")}</p>
+        <br />
 
-       
-     
+        <p className="text-3xl">{t("privacyPolicy.ToSSubTitle1")}</p>
+        <br />
+
+        <p
+          className=""
+          dangerouslySetInnerHTML={{
+            __html: t(`privacyPolicy.ToSText1`),
+          }}
+        />
+
+ <br />
+ <br />
+
+
+
+        <p className="text-3xl">{t("privacyPolicy.ToSSubTitle2")}</p>
+        <br />
+
+        <p
+          className=""
+          dangerouslySetInnerHTML={{
+            __html: t(`privacyPolicy.ToSText2`),
+          }}
+        />
+
+
       </div>
 
       <FooterClean />
