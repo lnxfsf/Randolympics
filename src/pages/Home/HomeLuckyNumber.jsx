231b21f2ff56a70da029034c3424a0bbfaa67482
lnxfsf <igor.lerinc7@gmail.com>
2025-01-08 07:39:27 +0100

    lucky number

--- src/pages/Home/HomeLuckyNumber.jsx
+++ src/pages/Home/HomeLuckyNumber.jsx
@@ -1,21 +1,50 @@
 import Button from "@mui/material/Button";
 import { useTranslation } from "react-i18next";
+import {useState} from "react";
 
 const HomeLuckyNumber = () => {
   const { t } = useTranslation();
 
+
+  const [luckyNumber, setLuckyNumber] = useState();
+  const [score, setScore] = useState(2);
+
+
+  
+
+  const finalResult = () => {
+
+    const amount = 100;
+    const supporters = 10;
+
+    setScore((amount*supporters*luckyNumber).toFixed(2)); 
+
+  }
+
+  const generateLuckyNumber = () => {
+
+    // between 1 and 4
+    const newRandomNumber = Math.random() * (4-1) + 1;
+
+    setLuckyNumber(newRandomNumber.toFixed(8));
+
+    finalResult();
+  }
+
+
+
+
   return (
     <>
-      <div className="flex items-start lexend-font text-black_second flex-col ">
+      <div className="flex items-center lexend-font text-black_second flex-col mt-8">
+        <p className="text-3xl md:text-4xl font-bold  text-center">
+          {t("home.howItWorks.content19")}
+        </p>
 
-
-
-
-
-
+        <p className="mt-4">{t("home.howItWorks.content20")}</p>
 
         <Button
-          onClick={() => {}}
+          onClick={generateLuckyNumber}
           className="self-center   w-[50%] "
           /* w-full md:w-50% */
           style={{ textTransform: "none" }}
@@ -35,12 +64,15 @@ const HomeLuckyNumber = () => {
           }}
         >
           <span className="lexend-font">{t("home.howItWorks.content21")}</span>
-
-
-          
         </Button>
 
 
+
+          <div className={`mt-8 ${luckyNumber ? "flex" : "hidden"} flex-col items-center gap-4 `}>
+            <p className="text-3xl md:text-4xl text-red_second ">{t("home.howItWorks.content22")}{" "}{luckyNumber}</p>
+
+            <p>{t("home.howItWorks.content23")}<span className="text-red_second">{t("home.howItWorks.content24")}</span> {t("home.howItWorks.content25")} <span className="text-red_second">{t("home.howItWorks.content26")}</span>{t("home.howItWorks.content27")}<span className="text-red_second">{luckyNumber}</span> = <span className="text-red_second">{score}</span>{t("home.howItWorks.content28")}</p>
+          </div>
 
 
       </div>
