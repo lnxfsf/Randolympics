9046661aadd2ca5232dfe87fb51e47dd87579b51
Igor Lerinc <igor.lerinc7@gmail.com>
2025-01-18 13:00:53 +0100

    remove french

--- languages.js
+++ languages.js
@@ -2,7 +2,6 @@
 
 export const lngs = {
     en: { nativeName: 'English' , translations: { en: 'English', sr: 'Engleski', fr: 'Anglais' } },
-    fr: { nativeName: 'French', translations: { en: 'French', sr: 'Francuski', fr: 'Français' } },
     sr: { nativeName: 'Srpski', translations: { en: 'Serbian', sr: 'Srpski', fr: 'Serbe' } },
 };
 
