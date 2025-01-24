2ec56d9b0a3328f651d1012bd5b12bd336d1442d
Igor Lerinc <igor.lerinc7@gmail.com>
2025-01-24 13:36:12 +0100

    add all balkan countries

--- src/context/supportedCountry.js
+++ src/context/supportedCountry.js
@@ -1,6 +1,6 @@
 
 
-var supportedCountry = ["US", "DE", "GB", "CN", "FR", "IT", "HU", "RU", "AU", "SE", "JP", "FI", "NO", "KR", "RO", "CA", "NL", "CU", "PL", "CH", "BG", "NZ", "ES", "BR", "BE", "DK", "UA", "KE", "TR", "ZA", "JM", "AR", "CZ", "AT", "IR", "GR", "BY", "UZ", "MX", "ET", "KP", "HR", "IE", "IN", "PK", "TH", "SK", "GE", "AZ", "PT", "UG", "CO", "TT", "NG", "VE", "ID", "MA", "TN", "DO", "EE", "LT", "EG", "TW", "SI", "ZW", "LV", "PH", "RS", "MN", "KZ", "AM", "DZ", "BS", "LU", "VN", "IS", "PE", "SG", "MY", "PR", "KG", "TJ", "HK", "XK", "AE", "SA", "BH", "QA", "LB", "JO", "CI", "GH", "SY", "MD", "MK", "IL"];
+var supportedCountry = ["US", "AL", "BA", "ME" , "DE", "GB", "CN", "FR", "IT", "HU", "RU", "AU", "SE", "JP", "FI", "NO", "KR", "RO", "CA", "NL", "CU", "PL", "CH", "BG", "NZ", "ES", "BR", "BE", "DK", "UA", "KE", "TR", "ZA", "JM", "AR", "CZ", "AT", "IR", "GR", "BY", "UZ", "MX", "ET", "KP", "HR", "IE", "IN", "PK", "TH", "SK", "GE", "AZ", "PT", "UG", "CO", "TT", "NG", "VE", "ID", "MA", "TN", "DO", "EE", "LT", "EG", "TW", "SI", "ZW", "LV", "PH", "RS", "MN", "KZ", "AM", "DZ", "BS", "LU", "VN", "IS", "PE", "SG", "MY", "PR", "KG", "TJ", "HK", "XK", "AE", "SA", "BH", "QA", "LB", "JO", "CI", "GH", "SY", "MD", "MK", "IL"];
 
 
 
