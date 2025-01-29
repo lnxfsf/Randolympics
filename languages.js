8fc894a2d9225ff945ce002e37256969ff2fc0ba
Igor Lerinc <igor.lerinc7@gmail.com>
2025-01-29 16:19:13 +0100

    croatian

--- languages.js
+++ languages.js
@@ -1,7 +1,8 @@
 
 
 export const lngs = {
-    en: { nativeName: 'English' , translations: { en: 'English', sr: 'Engleski', fr: 'Anglais' } },
-    sr: { nativeName: 'Srpski', translations: { en: 'Serbian', sr: 'Srpski', fr: 'Serbe' } },
+    en: { nativeName: 'English' , translations: { en: 'English', sr: 'Engleski', hr: 'Croatian' } },
+    sr: { nativeName: 'Srpski', translations: { en: 'Serbian', sr: 'Srpski', hr: 'Hrvatski' } },
+    hr: { nativeName: 'Hrvatski', translations: { en: 'Croatian', sr: 'Hrvatski', hr: 'Hrvatski' } },
 };
 
