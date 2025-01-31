b8eeaa74df80f516c23e2b89967eca8d14bf9f02
Igor Lerinc <igor.lerinc7@gmail.com>
2025-01-31 23:10:17 +0100

    milion debugger for faster

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
+import MillionLint from '@million/lint';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [MillionLint.vite({
+    enabled: true
+  }), react()],
   build: { sourcemap: false },
   optimizeDeps: {
     include: ['@mui/material/Tooltip'],
   },
-  esbuild: (process.env.VITE_NODE_ENV === "production" || process.env.VITE_NODE_ENV === "staging") ? {
+  esbuild: (process.env.VITE_NODE_ENV === 'production' || process.env.VITE_NODE_ENV === 'staging') ? {
     drop: ['console', 'debugger'],
   } : {}, 
  
