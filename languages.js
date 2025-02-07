b3f2d205f3b09d82c046a4eefaa7b384c688d4eb
Igor Lerinc <igor.lerinc7@gmail.com>
2025-02-07 13:44:28 +0100

    lng fix

--- languages.js
+++ languages.js
@@ -1,8 +1,8 @@
 
 
 export const lngs = {
-    en: { nativeName: 'English' , translations: { en: 'English', sr: 'Engleski', hr: 'Croatian' } },
-    sr: { nativeName: 'Srpski', translations: { en: 'Serbian', sr: 'Srpski', hr: 'Hrvatski' } },
+    en: { nativeName: 'English' , translations: { en: 'English', sr: 'Engleski', hr: 'Engleski' } },
+    sr: { nativeName: 'Srpski', translations: { en: 'Serbian', sr: 'Srpski', hr: 'Srpski' } },
     hr: { nativeName: 'Hrvatski', translations: { en: 'Croatian', sr: 'Hrvatski', hr: 'Hrvatski' } },
 };
 
